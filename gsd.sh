a936f8efeadc09b6679489c76447f2448e252ac1
John Cromartie <jcromartie@gmail.com>
2011-05-04 15:48:24 -0400

    extract default sites to separate variable

--- gsd.sh
+++ gsd.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # edit this list, or set GSD_SITES to add your custom sites
-SITES="$GSD_SITES reddit.com forums.somethingawful.com somethingawful.com digg.com break.com news.ycombinator.com infoq.com bebo.com twitter.com facebook.com blip.com youtube.com vimeo.com delicious.com flickr.com friendster.com hi5.com linkedin.com livejournal.com meetup.com myspace.com plurk.com stickam.com stumbleupon.com yelp.com slashdot.com"
+BASE_SITES="reddit.com forums.somethingawful.com somethingawful.com digg.com break.com news.ycombinator.com infoq.com bebo.com twitter.com facebook.com blip.com youtube.com vimeo.com delicious.com flickr.com friendster.com hi5.com linkedin.com livejournal.com meetup.com myspace.com plurk.com stickam.com stumbleupon.com yelp.com slashdot.com"
+
+SITES="$GSD_SITES $BASE_SITES"
 
 HOSTFILE="/etc/hosts"
 
